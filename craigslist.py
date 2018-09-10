c1d02932a803c636956d6ada6472eacffca7bc1f
Avy Faingezicht <avy@apple.com>
2018-09-09 17:36:31 -0700

    switch to guitars

--- craigslist.py
+++ craigslist.py
@@ -14,15 +14,13 @@ try:
 except Exception:
     r = {}
 
+SUBJECT = "Hi, maybe you can play one of these?"
 MAX_PID = r.get('last') or -1
 BASE_URL = 'http://sfbay.craigslist.org'
-PATH = '/search/apa'
-URL = BASE_URL + PATH
-with open('params.json') as f:
-    PARAMS = json.loads(f.read())
 
-env_params = json.loads(os.environ.get('PARAMS', "{}"))
-PARAMS.update(env_params)
+# for apartments this was "/search/apa/"
+PATH = '/search/sfc/sss'
+URL = BASE_URL + PATH
 
 # Remove weird characters
 USE_CHARS = string.ascii_letters + ''.join(str(i) for i in range(10)) + ' -$'
@@ -36,18 +34,21 @@ smtp_server.ehlo()
 smtp_server.login(EMAIL_USER, EMAIL_PASSWORD)
 
 
-def scrape_craigslist():
+def scrape_craigslist(params):
     cur_max_pid = MAX_PID
-    resp = requests.get(URL + PATH, params=PARAMS)
+    resp = requests.get(URL + PATH, params=params)
     soup = BeautifulSoup(resp.text, 'html.parser')
-    apts = soup.find_all(class_="result-row")
+    items = soup.find_all(class_="result-row")
 
-    for apt in apts:
-        title = apt.find('a', class_='hdrlnk')
+    for item in items:
+        title = item.find('a', class_='hdrlnk')
         name = ''.join(i for i in title.text if i in USE_CHARS)
         path = title.attrs['href']
-        pid = apt.get('data-pid')
-        if pid > MAX_PID and apt.get('data-repost-of') is None:
+        pid = item.get('data-pid')
+        if pid > MAX_PID \
+           and item.get('data-repost-of') is None \
+           and not 'squi' in name.lower() \
+           and not 'epiphone' in name.lower():
             cur_max_pid = max(cur_max_pid, pid)
             yield name, path
     try:
@@ -56,12 +57,12 @@ def scrape_craigslist():
         pass
 
 
-def assemble_email(apts_info):
-    apts = "\n".join("<li><a href={}{}>{}</a></li>".format(BASE_URL, path, name) for name, path in apts_info)
-    html = "<html><body><ul>{}</ul></html></body>".format(apts)
+def assemble_email(items_info):
+    items = "\n".join("<li><a href={}>{}</a></li>".format(path, name) for name, path in items_info)
+    html = "<html><body><ul>{}</ul></html></body>".format(items)
 
     msg = MIMEMultipart('alternative')
-    msg['Subject'] = "Hi, maybe we can live in one of these?"
+    msg['Subject'] = SUBJECT
     msg['From'] = EMAIL_USER
     msg['To'] = ', '.join(TO)
 
@@ -72,9 +73,12 @@ def assemble_email(apts_info):
 
 
 def main():
-    apts_info = list(scrape_craigslist())
-    if apts_info:
-        email_text = assemble_email(apts_info)
+    amps = list(scrape_craigslist({"query": 'vox+ac', "max_price": 500}))
+    teles = list(scrape_craigslist({"query": 'fender+telecaster', "max_price": 800}))
+    sg = list(scrape_craigslist({"query": 'gibson+sg', "max_price": 800}))
+    stuff = amps + [('', '</br></br>')] + teles + [('', '</br></br>')] + sg
+    if stuff:
+        email_text = assemble_email(stuff)
         smtp_server.sendmail(EMAIL_USER, TO, email_text)
         smtp_server.close()
 
